f77c6dfddde2fc5e1bf150e7be4b00c2b82881ba
octavian00 <vilceanuoctavian06@gmail.com>
2022-11-26 19:41:07 +0200

    allow frontend ip to acces data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://172.0.0.1:3000'
+
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
